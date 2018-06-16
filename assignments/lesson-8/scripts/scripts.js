a2b49b35fde0e9c315bf97d58c5d5f4f7c4ff2f5
niki1980 <38739103+niki1980@users.noreply.github.com>
2018-06-16 01:43:03 -0700

    Lesson 8
    Stormcenter and Thank you page

--- assignments/lesson-8/scripts/scripts.js
+++ assignments/lesson-8/scripts/scripts.js
@@ -2,3 +2,6 @@ function toggleMenu() {
     console.log(document.getElementById ("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
