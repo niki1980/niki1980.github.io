5dc61a3efbfa92f5fc839851930c842d3be95075
niki1980 <38739103+niki1980@users.noreply.github.com>
2018-06-30 22:27:50 -0700

    Lesson 11
    Gallery fixes

--- assignments/weathersite/scripts/windchill.js
+++
@@ -1,7 +0,0 @@
-let hightemp = parseFloat(document.getElementById("hightemp").innerHTML);
-let lowtemp = parseFloat(document.getElementById("lowtemp").innerHTML);
-let t = (hightemp+lowtemp)/2
-let s = parseFloat(document.getElementById("wind-speed").innerHTML);
-let wcfactor = 35.74 + (0.6215*t) - (35.75* Math.pow(s,0.16)) + (0.4275* t * Math.pow(s,0.16));
-let rounded = Math.round( wcfactor * 10 ) / 10;
-document.getElementById("windchillfactor").innerHTML = rounded
